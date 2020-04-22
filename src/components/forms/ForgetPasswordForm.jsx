1eb92bf0e3e62bf7cce63661e602c787e6bf0220
austinjan <austin.jan@gmail.com>
2020-04-22 17:59:12 +0800

    sent password response

--- src/components/forms/ForgetPasswordForm.jsx
+++ src/components/forms/ForgetPasswordForm.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Formik, Form, ErrorMessage, Field } from "formik";
 import { useForm } from "react-hook-form";
 import { connect } from "react-redux";
-import { Button, Input } from "antd";
+import { Button, Input, Modal } from "antd";
 import * as yup from "yup";
 import R from "ramda";
 import ErrorTip from "./ErrorTip";
@@ -14,31 +14,37 @@ const forgetPasswordForm = () => {
 
   const [response, checkEmail] = useSendPasswordEmail("users");
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
     checkEmail({ email: data.email });
   };
 
   useEffect(() => {
-    console.log("forgetpassword effect ", response);
-    if (response === "Not Found") {
+    if (response.result === "Password sent") {
+      Modal.success({ content: response.result });
+    } else if (response.result != undefined) {
+      Modal.error({ content: response.result });
     }
   }, [response]);
 
+  const handleOk = (e) => {
+    setModalVisible(false);
+  };
+
   return (
-    <form className="container_col" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-row">
-        <label htmlFor="email">登入 Email: </label>
-        <input name="email" ref={register({ required: true })} />
-      </div>
-      <div className="form-row">
-        {errors.email && <p>This field is required</p>}
-      </div>
-      <div className="form-row">
-        {response ? <span>{response}</span> : null}
-      </div>
-      <Button htmlType="submit"> submit</Button>
-    </form>
+    <div>
+      <form className="container_col" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-row">
+          <label htmlFor="email">登入 Email: </label>
+          <input name="email" ref={register({ required: true })} />
+        </div>
+        <div className="form-row">
+          {errors.email && <p>This field is required</p>}
+        </div>
+
+        <Button htmlType="submit"> submit</Button>
+      </form>
+    </div>
   );
 };
 
