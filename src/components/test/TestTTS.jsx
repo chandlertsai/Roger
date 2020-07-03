e99e6ed8b933f5bc2b766d29e44cabbda9d1c9ad
austinjan <austin.jan@gmail.com>
2020-07-03 09:54:05 +0800

    Add purge action

--- src/components/test/TestTTS.jsx
+++ src/components/test/TestTTS.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { Input, Button } from "antd";
-
+import { useDispatch } from "react-redux";
+import { purge } from "actions/appState";
 const testTTS = () => {
   const [text, setText] = useState("");
-
+  const dispatch = useDispatch();
   const speak = () => {
     var words = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(words);
+  };
+
+  const handlePurge = () => {
+    dispatch(purge());
   };
 
   const textChanged = ({ target: { value } }) => {
@@ -15,11 +20,12 @@ const testTTS = () => {
   return (
     <div>
       <Input.TextArea
-        placeholder='輸入要說的文字'
+        placeholder="輸入要說的文字"
         value={text}
         onChange={textChanged}
       />
       <Button onClick={speak}> Speak</Button>
+      <Button onClick={handlePurge}> purge</Button>
     </div>
   );
 };
