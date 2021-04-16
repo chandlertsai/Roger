78c93bdf8140d8783679431d5b4ddcb28ef78294
Austin Jan <austin.jan@gmail.com>
2021-04-16 15:03:26 +0800

    LicenseMessage change data sourcing

--- src/components/pureComponents/licenseMessage.jsx
+++ src/components/pureComponents/licenseMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useFetch } from "apis/crud";
+
 import { useTranslation } from "react-i18next";
+import { useLicense } from "apis/license";
 import { Table } from "antd";
 import R from "ramda";
 
 const licenseMessage = () => {
-  const [lic] = useFetch("licenseMessage");
+  const lic = useLicense();
   const { t } = useTranslation();
   // const licenseItems = lic => {
   //   const license = R.compose(R.prop("license"), R.head)(lic);
@@ -13,29 +14,34 @@ const licenseMessage = () => {
   //     <li class="list-group-item">{t("licenseMessage")}</li>
   //   ,license)
   // };
+  console.log("lic ", lic, R.path(["information", "owner"], lic));
   const columns = [
     {
       title: t("licenseMessage.expire"),
       dataIndex: "expire",
-      key: "expire"
+      key: "expire",
     },
     {
       title: t("licenseMessage.count"),
       dataIndex: "count",
-      key: "count"
-    }
+      key: "count",
+    },
   ];
 
   return (
     <div className="card p-2">
       <h5 className="card-title"> {t("licenseMessage.title")} </h5>
       <h6 className="card-text">
-        {t("licenseMessage.company")} : {R.prop("company", R.head(lic))}
+        {t("licenseMessage.company")} : {R.path(["information", "owner"], lic)}
       </h6>
 
-      <Table dataSource={R.prop("license", R.head(lic))} columns={columns} />
+      <Table
+        dataSource={R.path(["information", "licenses"], lic)}
+        columns={columns}
+      />
       <p className="card-text">
-        {t("licenseMessage.contact")} : {R.prop("support", R.head(lic))}
+        {t("licenseMessage.contact")} :{" "}
+        {R.path(["information", "contact"], lic)}
       </p>
     </div>
   );
